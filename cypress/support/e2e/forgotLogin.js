24633e7fc3ac926375b6acdf2a394f4c378353b7
Whardah <whardah25@gmail.com>
2022-12-03 21:02:44 +0700

    Forgot login with incorrect data inputted

--- cypress/support/e2e/forgotLogin.js
+++ cypress/support/e2e/forgotLogin.js
@@ -8,6 +8,22 @@ describe("Para Bank Test Authentication", () => {
 });
 
 describe("Forgot Login Feature", () => {
+  it("Verify get login info by input incorrect user data details", () => {
+    cy.contains("Forgot login info?").click();
+    //Input Forgot Login Form
+    cy.get(element.forgot.firstName).type(data.invalidUser.password);
+    cy.get(element.forgot.lastName).type(data.invalidUser.password);
+    cy.get(element.forgot.address).type(data.invalidUser.username);
+    cy.get(element.forgot.city).type(data.invalidUser.username);
+    cy.get(element.forgot.state).type(data.invalidUser.password);
+    cy.get(element.forgot.zipCode).type(data.invalidUser.password);
+    cy.get(element.forgot.ssn).type(data.invalidUser.password);
+
+    cy.get(element.button).contains(element.forgot.btnForgot).click();
+
+    //Successfully displayed username and password
+    cy.contains("The customer information provided could not be found.");
+  });
   it("Verify get valid username and password", () => {
     cy.contains("Forgot login info?").click();
     //Input Forgot Login Form
