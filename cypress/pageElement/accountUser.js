fc2be19561cbfccd0e1ccbdb665a75f2fc4db970
Whardah <whardah25@gmail.com>
2022-12-03 14:37:35 +0700

    pageElement accountUser

--- cypress/pageElement/accountUser.js
+++ cypress/pageElement/accountUser.js
@@ -5,3 +5,18 @@ export const login = {
 };
 
 export const button = "input.button";
+
+export const register = {
+  firstName: 'input[name="customer.firstName"]',
+  lastName: 'input[name="customer.lastName"]',
+  address: 'input[name="customer.address.street"]',
+  city: 'input[name="customer.address.city"]',
+  state: 'input[name="customer.address.state"]',
+  zipCode: 'input[name="customer.address.zipCode"]',
+  phone: 'input[name="customer.phoneNumber"]',
+  ssn: 'input[name="customer.ssn"]',
+  username: 'input[name="customer.username"]',
+  password: 'input[name="customer.password"]',
+  confirm: 'input[name="repeatedPassword',
+  btnRegister: "Register",
+};
