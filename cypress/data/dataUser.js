c5da6680980fbde6b2c3cc91827bb1b78bc63093
Whardah <whardah25@gmail.com>
2022-12-03 15:11:20 +0700

    dataUser

---
+++ cypress/data/dataUser.js
@@ -0,0 +1,24 @@
+export const validUser = {
+  username: "whardah",
+  password: "password",
+};
+
+export const invalidUser = {
+  username: "whardah",
+  password: "123",
+};
+
+export const userData = {
+  firstName: "Whardah",
+  lastName: "Ksm",
+  address: "Sesame St",
+  city: "Seoul",
+  state: "Korea",
+  zipCode: "3000",
+  phone: "012345678",
+  ssn: "123987",
+  username: "whardah",
+  password: "password",
+  validConfirm: "password",
+  invalidConfirm: "123",
+};
